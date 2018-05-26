05c0218cc274fe881aab76425df234b79d830175
freeunit <tel1354505@yandex.com>
2018-05-26 22:12:58 +0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
     print ( "Waiting ...")
+    -- Делаем задержку на загрузку файла, чтоб если что не так успеть перезаписаться
 tmr.register(0, 5000, tmr.ALARM_SINGLE, 
     function (t)
     tmr.unregister (0);
