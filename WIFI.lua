45870dac7854a0998aae3052072fd38aebd7d432
freeunit <tel1354505@yandex.com>
2018-05-27 15:23:26 +0500

    Update WIFI.lua

--- WIFI.lua
+++ WIFI.lua
@@ -1,11 +1,11 @@
 -- Настройка вай фай в режиме ведомой со статическим адресом
 wifi.setmode(wifi.STATION)
 cfg={}
-cfg.ssid="TPS"
-cfg.pwd="poporing4516(root)!"
+cfg.ssid="WIFI"
+cfg.pwd="password"
 wifi.sta.config(cfg)
 cfg={}
-cfg.ip="192.168.0.5"
+cfg.ip="192.168.0.2"
 cfg.netmask= "255.255.255.0"
-cfg.gateway="192.168.0.2"
+cfg.gateway="192.168.0.1"
 wifi.sta.setip(cfg)
